04592aa9c9abb16c086c8a32b0b4651dbc767f41
Charlie Polonus <capolonus@gmail.com>
2023-02-13 14:49:24 -0600

    Quick simplicfication

--- Assets/Scripts/Managers/BuildingManager.cs
+++ Assets/Scripts/Managers/BuildingManager.cs
@@ -14,6 +14,7 @@ public class BuildingManager : MonoBehaviour
     [SerializeField]
     private Vector3 _offset;
 
+    //Ignore, for script management
     private void Awake()
     {
         if (main == null)
@@ -28,23 +29,41 @@ public class BuildingManager : MonoBehaviour
 
     private void Start()
     {
+        //Creates an empty list of the buildings to be creates
         _buildings = new();
     }
 
+    /// <summary>
+    /// Instantiates a new building with BuildingBehaviour at index (x,y), and changes MapData to cpmensate for it
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
     public void CreateBuilding(int x, int y)
     {
+        //Creates the new building object
         transform.position = new Vector3(x, 0, y) + _offset;
         _buildings.Add(Instantiate(_buildingObj, transform.position, Quaternion.identity));
+
+        //Edits the map
         MapManager.main.EditGrid(x, y, -1);
+
+        //Edits the pathing of all visible enemies
         EnemyManager.main.UpdatePaths();
     }
 
+    /// <summary>
+    /// Removes a building and the corresponding BuildingBehaviour at index (x,y), and changes MapData to cpmensate for it
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
     public void DestroyBuilding(int x, int y)
     {
+        //Find the building at the location of (x,y)
         foreach (BuildingBehaviour curBehaviour in _buildings.ToArray())
         {
             if (Mathf.RoundToInt(curBehaviour.transform.position.x) == x && Mathf.RoundToInt(curBehaviour.transform.position.z) == y)
             {
+                //Edits the grid and removes the building
                 MapManager.main.EditGrid(x, y, 0);
                 Destroy(curBehaviour.gameObject);
                 _buildings.Remove(curBehaviour);
